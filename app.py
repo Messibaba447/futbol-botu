7da7de54e130b2e700b3e3c1f3c0339bd5022956
Messibaba447 <fulyakilic123456@gmail.com>
2026-04-08 20:04:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if "GOOGLE_API_KEY" in st.secrets:
     genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 # MODEL İSMİ BURADA: En sorunsuz ismi yazdım
-model = genai.GenerativeModel('models/gemini-pro')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 prompt = st.text_input("Sorunuzu yazın:")
 
